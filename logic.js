c379c192082fe7fb5485f29cd246310b510d8b3d
Kittur Sameer <42382545+vacool6@users.noreply.github.com>
2022-11-03 08:59:57 +0530

    feat/chat room

---
+++ logic.js
@@ -0,0 +1,39 @@
+const sendBtn = document.querySelector("#send");
+const messages = document.querySelector("#messages");
+const messageBox = document.querySelector("#inputBox");
+
+let ws;
+
+function showMessage(message) {
+  messages.textContent += `\n> ${message}`;
+  messages.scrollTop = messages.scrollHeight;
+  messageBox.value = "";
+}
+
+function init() {
+  if (ws) {
+    ws.onerror = ws.onopen = ws.onclose = null;
+    ws.close();
+  }
+
+  ws = new WebSocket("ws://localhost:8080");
+  ws.onopen = () => {
+    console.log("Connection opened!");
+  };
+  ws.onmessage = ({ data }) => showMessage(data);
+  ws.onclose = function () {
+    ws = null;
+  };
+}
+
+sendBtn.onclick = function () {
+  if (!ws) {
+    showMessage("No WebSocket connection :(");
+    return;
+  }
+
+  ws.send(messageBox.value);
+  showMessage(messageBox.value);
+};
+
+init();
